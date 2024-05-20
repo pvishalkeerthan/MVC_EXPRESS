9065f66cf66a88e1056c501772fd6628fa8ef5aa
Your Name <you@example.com>
2024-05-20 11:31:37 +0530

    v2

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,7 +1,7 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const flash=require('connect-flash')
+//const flash=require('connect-flash')
 
 exports.getLogin = (req, res) => {
     res.render('login');
@@ -12,16 +12,16 @@ exports.postLogin = async (req, res) => {
     try {
         const user = await User.findOne({ username });
         if (!user || !(await bcrypt.compare(password, user.password))) {
-            req.flash('error', 'Invalid username or password');
+           // req.flash('error', 'Invalid username or password');
             return res.render('login', { error: 'Invalid username or password' });
         }
         const token = jwt.sign({ userId: user._id }, "123");
         res.cookie('jwt', token, { httpOnly: true });
-        req.flash('success', 'Login successful!');
+       // req.flash('success', 'Login successful!');
         res.redirect('/home');
         console.log('Login successful');
     } catch (err) {
-        req.flash('error', 'Error in login');
+       // req.flash('error', 'Error in login');
         res.status(500).send('Error in login');
     }
 };
@@ -36,11 +36,11 @@ exports.postRegister = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = new User({ username, password: hashedPassword });
         await newUser.save();
-        req.flash('success', 'Registration successful!');
+       // req.flash('success', 'Registration successful!');
         res.render("../views/login");
         console.log('Registration successful');
     } catch (err) {
-        req.flash('error', 'Error in registration');
+       // req.flash('error', 'Error in registration');
         res.status(500).send('Error in registration');
     }
 };
